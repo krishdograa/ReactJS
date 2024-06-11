801e31dc92cc48c845751061e43a5a9c2220f08b
krishdograa <155013010+krishdograa@users.noreply.github.com>
2024-06-11 17:28:55 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,12 @@
+import  './Headerstyle.css'
+
+function Footer(){
+    return(
+        <div id = "foot">
+         2024      Company      Report a Problem
+        </div>
+        
+    );
+}
+
+export default Footer;
