24d5a89b94bd94a0e39d0723a2605a62c0786afe
krishdograa <155013010+krishdograa@users.noreply.github.com>
2024-06-11 17:26:18 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+// import Signup from './Pages/Signup'
+import Signup from './Pages/Signup'
+import Startingpage from './Pages/Startingpage'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import App from './Practice/App'
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  
+    <App/>
+    
+   
+  </React.StrictMode>,
+)
